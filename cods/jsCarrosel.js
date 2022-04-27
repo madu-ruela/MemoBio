253b13af5ff5f74d3ad7c5d0af530fc3e7d76e38
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-04-27 15:19:08 -0300

    Alterando carrossel

--- cods/jsCarrosel.js
+++ cods/jsCarrosel.js
@@ -1,11 +1,19 @@
 const btnAnterior = document.querySelector(".anterior")
 const btnProximo = document.querySelector(".proximo")
-const cardDep = document.querySelector(".dep")
+
+let prox = 0;
+const container = document.querySelector(".testeCarrosel")
+const card = `
+<div>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxS4D04XbJPmPFWIvVh4YLHLnoV3bIk2x5rjgtZzxqJrxDdlJo-QjcHClpjLXOsSuqMA0&usqp=CAU">
+    <h3>Nome</h3>
+    <p>depoimento....depoimento....depoimento....depoimento....</p>
+</div>`
 
 btnAnterior.addEventListener("click", () => {
-    console.log("antes")
+    //console.log("antes")
 })
 
 btnProximo.addEventListener("click", () => {
-    console.log("proximo")
+    //console.log("proximo")
 })
