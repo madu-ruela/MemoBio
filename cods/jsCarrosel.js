aa6992ad633065bedcf50b41bb1b6401d57f15d6
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-04-27 14:51:38 -0300

    Teste Carrossel

--- cods/jsCarrosel.js
+++ cods/jsCarrosel.js
@@ -2,6 +2,10 @@ const btnAnterior = document.querySelector(".anterior")
 const btnProximo = document.querySelector(".proximo")
 const cardDep = document.querySelector(".dep")
 
-function numeracaoID(){
-	
-}
+btnAnterior.addEventListener("click", () => {
+    console.log("antes")
+})
+
+btnProximo.addEventListener("click", () => {
+    console.log("proximo")
+})
