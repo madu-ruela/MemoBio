c8e008ac78d6e1307a247a653c62a4b0b4f987a6
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-04-27 13:20:51 -0300

    Teste Carrossel dos Depoimentos

---
+++ cods/jsCarrosel.js
@@ -0,0 +1,7 @@
+const btnAnterior = document.querySelector(".anterior")
+const btnProximo = document.querySelector(".proximo")
+const cardDep = document.querySelector(".dep")
+
+function numeracaoID(){
+	
+}
